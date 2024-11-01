3274573277a1123a571b8ef8da675accf2d11825
CharZX1 <sonicadvanced21@hotmail.com>
2024-10-31 23:27:12 -0700

    Made a JS file that makes use of the different types of variables.

---
+++ JavaScriptVariables/index.js
@@ -0,0 +1,45 @@
+// Using let to declare a variable 
+let age = 25;
+console.log(age); // Expected Output: 25
+
+// Using const to declare a variable
+const birthYear = 1998;
+console.log(birthYear); // Expected Output: 1998
+
+// Making console logs readable
+console.log("Initial Age:", age); // Expected Output: Initial Age: 25
+
+console.log("Birth Year:", birthYear); // Expected Output: Birth Year: 1998
+
+// Modify let variable 
+age = 30;
+console.log("Updated Age: ", age); // Expected Output: Updated Age: 30
+
+// Attempt to modify the const variable birthYear 
+// birthYear = 2000; // Error: Assignment to constant variable.
+
+console.log("birthYear: ", birthYear); // Expected Output: Birth Year: 1998
+
+// Variables in Expressions 
+let currentYear = 2024;
+yearsSinceBirth = currentYear - birthYear;
+console.log("Years Since Birth: ", yearsSinceBirth); // Expected Output 26
+
+// Update the current year
+
+currentYear = 2025;
+yearsSinceBirth = currentYear - birthYear;
+console.log("Years Since Birth After Update: ", yearsSinceBirth); // Expected output: 27
+
+// Dynamic Output with strings and variables
+const greeting = "Hello";
+let userName = "Alice"
+let personalizedGreeting = greeting + ", " + userName + "!";
+console.log(personalizedGreeting); // Expected Output: Hello, Alice
+
+// Updated the username
+userName = "Bob";
+personalizedGreeting = greeting + ", " + userName + "!";
+console.log("Update Greeting: ", personalizedGreeting); // Expected Output: Hello, Bob!
+
+
